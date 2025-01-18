d9c1131c8b845a569a9c09e5306a1561ec5194e0
mouad <152200815+mouad2008dev@users.noreply.github.com>
2025-01-18 13:05:40 +0100

    Update lenght calculate.c
    add the lenght calculator to a function

--- lenght calculate.c
+++ lenght calculate.c
@@ -3,13 +3,17 @@
 #include"cs50.h"
 
 int main(void){
-      string s = get_string("typr : ");
+
+      string name = get_string("typr : ");
+      int lenght = get_lenght(name);
+      printf("lenght: %i\n",lenght);
+      
+}
+int get_lenght(string s){
       int i =0;
       while (s[i] != '\0')
       {
             i++;
       }
-      printf("lenght: %i\n",i);
-      
+      return i;
 }
- 
