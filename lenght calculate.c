bfee3613ffe757ef8c365593c4d6ed3fef9f4ec3
mouad <152200815+mouad2008dev@users.noreply.github.com>
2025-01-18 12:49:09 +0100

    Add files via upload

---
+++ lenght calculate.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#include"cs50.h"
+
+int main(void){
+      string s = get_string("typr : ");
+      int i =0;
+      while (s[i] != '\0')
+      {
+            i++;
+      }
+      printf("lenght: %i\n",i);
+      
+}
+ 
